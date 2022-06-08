be99177901d6d2ac318786a92a248e47ccc7769a
Sai Prasad M <saiprasad-m>
2022-06-08 16:57:23 +0530

    Adding EC2 with AMI

--- main.tf
+++ main.tf
@@ -68,25 +68,25 @@ resource "aws_security_group" "mtc_sg" {
 }
 
 resource "aws_key_pair" "mtc_auth" {
-    key_name = "mtckey"
-    public_key = file("~/.ssh/mtckey.pub")
-  
+  key_name   = "mtckey"
+  public_key = file("~/.ssh/mtckey.pub")
+
 }
 
 resource "aws_instance" "dev_node" {
-    instance_type = "t2.micro"
-    ami = data.aws_ami.dev_server_ami.id
+  instance_type = "t2.micro"
+  ami           = data.aws_ami.dev_server_ami.id
 
-    tags = {
-        Name = "dev-node"
-    }
+  tags = {
+    Name = "dev-node"
+  }
 
-    key_name = aws_key_pair.mtc_auth.id
-    vpc_security_group_ids = [ aws_security_group.mtc_sg.id ]
-    subnet_id = aws_subnet.mtc_public_subnet.id
+  key_name               = aws_key_pair.mtc_auth.id
+  vpc_security_group_ids = [aws_security_group.mtc_sg.id]
+  subnet_id              = aws_subnet.mtc_public_subnet.id
 
-    root_block_device {
-      volume_size = 10
-    }
+  root_block_device {
+    volume_size = 10
+  }
 
 }
